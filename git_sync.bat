754a001b5014466f9ce8e431f1999d25b0746fda
Qianban2027 <qianban2027@outlook.com>
2024-12-30 13:37:30 +0800

    committed by git_sync.bat

--- git_sync.bat
+++ git_sync.bat
@@ -1,15 +1,12 @@
 @echo off
 
 
-echo Pulling latest changes from %BRANCH% branch...
 git pull origin main
 
-echo Adding changes to staging...
 git add .
 
 git commit -m "committed by git_sync.bat"
 
-echo Pushing changes to main branch...
 git push origin main
 
 echo Git sync complete!
